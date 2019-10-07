53c40a9e223ce8e653e22714092829e89a9aa599
unknown <cormacmchale1@gmail.com>
2019-10-07 12:51:37 +0100

    basic start menu added

---
+++ BadDrivers/Assets/Scripts/startGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startGame : MonoBehaviour
+{
+    //dont need to perform tasks on frame refresh in the script
+    public void startLevel(string sceneName)
+    {
+        //load the first level when you press the start button
+        SceneManager.LoadScene(sceneName);
+    }
+}
