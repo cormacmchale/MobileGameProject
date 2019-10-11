793f46aa3d7b27edac085df9e8bd697f3fd77073
unknown <cormacmchale1@gmail.com>
2019-10-11 16:23:58 +0100

    firing and movement complete

--- BadDrivers/Assets/Scripts/fireBullet.cs
+++ BadDrivers/Assets/Scripts/fireBullet.cs
@@ -6,51 +6,53 @@ public class fireBullet : MonoBehaviour
 {
     //placeholder for the bullet prefab 
     public bullet bullet;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     // Update is called once per frame
     void Update()
     {
-        //check which direction the player is moving
-
-        //spawn bullet in the direction
-        //when player presses space
+        //when player presses fire
         if (Input.GetKeyDown("space"))
         {
-            //give the bullet the position of the player
-            //spawn the bullet in
-            //these will now be called elsewhere
-            //bullet.transform.position = transform.position;
-            //Instantiate(bullet);
-            bulletDirection(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
+            //call method to fire a bullet
+            firebullet();
         }
     }
-    void bulletDirection(float Xmovement, float Ymovement)
+    void firebullet()
     {
-        //transform the bullet in whatever diection the car is moving
-        //just check these values to know which direction
-            //testing
-            //Debug.Log(Xmovement+" "+Ymovement);
-        //keeping this here for now
-        //give the bullet a position
-        //and create the game object
-        if (Xmovement < 0)
-        {
-            bullet.transform.position = transform.position;
-            bullet.transform.rotation = new Quaternion(0f, 45f, 0f, 0f);
-            Instantiate(bullet);
-        }
-        //shoot left
-        else
-        {
-            bullet.transform.position = transform.position;
-            bullet.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
-            Instantiate(bullet);
-        }
+        //put the bullet back to normal before you add correct rotation
+        bullet.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        //return a value for direction
+        int setDirection = getDirection();
+        //Debug.Log(setDirection);
+        //depending on direction value add correct rotation
+        shoot(setDirection);
+    }
+    //method that reurns a single value to be used to configure dirction the bullet should fire
+    int getDirection()
+    {
+        //moving right along the X axis-- shoot straight
+        if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") == 0) return 0;
+        //moving right along the x axis and up along the y axis -- shoot up left
+        else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") > 0) return 45;
+        //moving up the Y axis only - more lee-way added in for x -- shoot up
+        else if (Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") == 0) return 90;
+        //moving right along the x axis and up along the y axis -- shoot down right
+        else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") < 0) return -45;
+        //moving down the Y axis only - more lee-way added in for x -- shoot down
+        else if (Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") == 0) return -90;
+        //moving backwards across the x-axis --shoot left
+        else if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") < 0) return -180;
+        //moving backwards accross the x axis and down the y --shoot down left
+        else if (Input.GetAxis("Vertical") < 0 && Input.GetAxis("Horizontal") < 0) return -135;
+        //moving backwards accross the x axis and up the y --shoot down left
+        else if (Input.GetAxis("Vertical") > 0 && Input.GetAxis("Horizontal") < 0) return -215;
+        //default - shoot straight
+        else return 0;
+    }
+    //gives the bullet the correct position and calculated angle and instantiates the bullet for firing
+    void shoot(int angle)
+    {
+        bullet.transform.position = transform.position;
+        bullet.transform.Rotate(Vector3.forward * angle);
+        Instantiate(bullet);
     }
 }
