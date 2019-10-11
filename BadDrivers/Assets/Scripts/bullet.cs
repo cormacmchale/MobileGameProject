dfe1a3bf0f5dac0a434a0a24a229f9b5240bc0c8
unknown <cormacmchale1@gmail.com>
2019-10-11 16:52:13 +0100

    initial enemy destruction implemented

--- BadDrivers/Assets/Scripts/bullet.cs
+++ BadDrivers/Assets/Scripts/bullet.cs
@@ -8,6 +8,7 @@ public class bullet : MonoBehaviour
     [SerializeField]
     private float thrust = 10.0f;
     public Rigidbody2D shoot;
+    //private Collision2D = GameObject.getComponent<On>;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,25 @@ public class bullet : MonoBehaviour
         //will control direction of bullet based of the direction the player is moving in
         //will not need anything else here
         shoot.velocity = transform.right * thrust;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (col.gameObject.name == "player")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Debug.Log("Do Nothing");
+        }
+        else
+        {
+            Debug.Log("Enemey Destroyed");
+            //destroy everything
+            //update score here??
+            //also check for ambulance
+            Destroy(gameObject);
+            Destroy(col.gameObject);
+        }
     }
 
 }
