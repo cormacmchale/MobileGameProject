fead7d0c4659e1b826f089b50d24a057c53ff756
unknown <cormacmchale1@gmail.com>
2019-11-27 12:01:32 +0000

    sfx toggle added

---
+++ BadDrivers/Assets/Scripts/utils/sfxControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sfxControl : MonoBehaviour
+{
+    //access the toggle on screen
+    [SerializeField]
+    private Toggle toggle;
+
+    //access the audio manager to change settings
+    private AudioManager music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = FindObjectOfType<AudioManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(toggle.isOn);
+    }
+}
